4b9b8c091c93c5b794cc4f88937a8a0b10670bd8
wefspy <126880105+wefspy@users.noreply.github.com>
2025-05-24 19:44:50 +0500

    feat: add repositories with crud operations

---
+++ src/main/java/ru/wefspy/AssessmentProfessionallyQualities/infrastructure/repository/JdbcUserRoleRepository.java
@@ -0,0 +1,81 @@
+package ru.wefspy.AssessmentProfessionallyQualities.infrastructure.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.wefspy.AssessmentProfessionallyQualities.domain.model.UserRole;
+import ru.wefspy.AssessmentProfessionallyQualities.infrastructure.mapper.UserRoleRowMapper;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcUserRoleRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final UserRoleRowMapper userRoleRowMapper;
+
+    public JdbcUserRoleRepository(JdbcTemplate jdbcTemplate,
+                                  UserRoleRowMapper userRoleRowMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.userRoleRowMapper = userRoleRowMapper;
+    }
+
+    public Long count() {
+        return jdbcTemplate.queryForObject("SELECT count(*) FROM users_roles ", Long.class);
+    }
+
+    public UserRole save(UserRole userRole) {
+        return jdbcTemplate.queryForObject(
+                "INSERT INTO users_roles (user_id, role_id) " +
+                        "VALUES (?, ?) " +
+                        "RETURNING * ",
+                userRoleRowMapper,
+                userRole.getUserId(),
+                userRole.getRoleId()
+        );
+    }
+
+    public void saveAll(Collection<UserRole> usersRoles) {
+        jdbcTemplate.batchUpdate(
+                "INSERT INTO users_roles (user_id, role_id) " +
+                        "VALUES (?, ?) ",
+                usersRoles,
+                usersRoles.size(),
+                (ps, userRole) -> {
+                    ps.setLong(1, userRole.getUserId());
+                    ps.setLong(2, userRole.getRoleId());
+                }
+        );
+    }
+
+    public Optional<UserRole> findById(Long id) {
+        List<UserRole> usersRoles = jdbcTemplate.query(
+                "SELECT * " +
+                        "FROM users_roles " +
+                        "WHERE id = ? ",
+                userRoleRowMapper,
+                id
+        );
+
+        return usersRoles.stream().findFirst();
+    }
+
+    public UserRole update(UserRole userRole) {
+        jdbcTemplate.update(
+                "UPDATE users_roles " +
+                        "SET " +
+                        "user_id = ?, " +
+                        "role_id = ? " +
+                        "WHERE id = ?",
+                userRole.getUserId(),
+                userRole.getRoleId(),
+                userRole.getId()
+        );
+
+        return userRole;
+    }
+
+    public void delete(Long id) {
+        jdbcTemplate.update("DELETE FROM users_roles WHERE id = ?", id);
+    }
+}
