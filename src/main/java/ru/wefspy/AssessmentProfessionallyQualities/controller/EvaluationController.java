835599fde4da15e4c03363eb86e769c7a06b18f1
wefspy <126880105+wefspy@users.noreply.github.com>
2025-05-26 00:20:01 +0500

    refactor: transfer EvaluationController

--- src/main/java/ru/wefspy/AssessmentProfessionallyQualities/controller/EvaluationController.java
+++ src/main/java/ru/wefspy/AssessmentProfessionallyQualities/controller/EvaluationController.java
@@ -1,4 +1,4 @@
-package ru.wefspy.AssessmentProfessionallyQualities.application.controller;
+package ru.wefspy.AssessmentProfessionallyQualities.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
